206440ffbacc01970987284c213a69497cb43030
Bluemorpho61 <vanda10.dava@gmail.com>
2024-09-17 14:03:22 +0700

    init

---
+++ app/src/main/java/com/alkindi/gcg_akor/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.alkindi.gcg_akor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.alkindi.gcg_akor.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view =binding.root
+        enableEdgeToEdge()
+        setContentView(view)
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
