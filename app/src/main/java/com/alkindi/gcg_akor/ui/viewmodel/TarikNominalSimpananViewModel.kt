5a105df0af342827db8210aa46a4392480ced3b1
Bluemorpho61 <vanda10.dava@gmail.com>
2024-11-07 09:50:46 +0700

    fitur insert ke table mbr_pull sdh work

--- app/src/main/java/com/alkindi/gcg_akor/ui/viewmodel/TarikNominalSimpananViewModel.kt
+++ app/src/main/java/com/alkindi/gcg_akor/ui/viewmodel/TarikNominalSimpananViewModel.kt
@@ -11,7 +11,6 @@ import com.alkindi.gcg_akor.data.remote.response.TarikNominalSimpananResponse
 import com.alkindi.gcg_akor.data.remote.retrofit.ApiConfig
 import com.alkindi.gcg_akor.data.repository.UserRepository
 import kotlinx.coroutines.launch
-import java.net.URLEncoder
 
 class TarikNominalSimpananViewModel(private val userRepository: UserRepository) : ViewModel() {
     private val _tarikNominalSimpananResponse = MutableLiveData<TarikNominalSimpananResponse>()
@@ -33,29 +32,38 @@ class TarikNominalSimpananViewModel(private val userRepository: UserRepository) 
     ) {
         if (nominalSimpanan.isEmpty() && tipeSimpanan.isEmpty()) {
             Log.e(TAG, "Failed to make a tarik simpnanan request")
+            return
         } else {
+            val argl = """{
+                    "mbrid":"$mbrid",
+                    "trans_date":"$transDate",
+                    "amount":"$nominalSimpanan",
+                    "doc_date":"$transDate",
+                    "stp":"$tipeSimpanan",
+                    "txn_amount":"$simpananYgTersedia",
+                    "ket":"$catatan"
+                    }""".trimIndent()
             try {
                 _isLoading.value = true
-//                val apiService = ApiConfig.getApiService()
                 val responseCode =
                     "NU5mgOhAZUGhJ24WH1zuqwTnRtBFfK6y6OVw0Q2/ZWSE2T%2BDBSLsen/SgBttLGZS"
-//                val argt = "vars"
-                val argVal = URLEncoder.encode(
-                    "{\"mbrid\": \"${mbrid}\", \"trans_date\":\"${transDate}\", \"amount\":\"${nominalSimpanan}\",\"doc_date\":\"${transDate}\",\"stp\":\"${tipeSimpanan}\",\"txn_amount\":\"${simpananYgTersedia}\",\"ket\":\"${catatan}\"",
-                    "UTF-8"
-                )
+//                val argVal = URLEncoder.encode(
+//                    "{\"mbrid\": \"${mbrid}\", \"trans_date\":\"${transDate}\", \"amount\":\"${nominalSimpanan}\",\"doc_date\":\"${transDate}\",\"stp\":\"${tipeSimpanan}\",\"txn_amount\":\"${simpananYgTersedia}\",\"ket\":\"${catatan}\"",
+//                    "UTF-8"
+//                )
                 val fullUrl =
-                    "${ApiConfig.BASE_URL_KOPEGMAR}txn?fnc=runLib;opic=${ApiConfig.API_DEV_CODE_KOPEGMAR};csn=${ApiConfig.WORKSPACE_CODE_KOPEGMAR};rc=${responseCode};vars=${argVal}"
+                    "${ApiConfig.BASE_URL_KOPEGMAR}txn?fnc=runLib;opic=${ApiConfig.API_DEV_CODE_KOPEGMAR};csn=${ApiConfig.WORKSPACE_CODE_KOPEGMAR};rc=${responseCode}"
                 Log.d(
                     TAG,
                     "Data yang dimasukkan: (MBRID: $mbrid, nominal yg ditarik: $nominalSimpanan, catatan: $catatan, tipe simpanan: $tipeSimpanan, simpanan yang tersedia: $simpananYgTersedia, transDate: $transDate)"
                 )
                 viewModelScope.launch {
-                    val response = ApiConfig.getApiService().postTarikSimpanan(argl = argVal)
+                    val response = ApiConfig.getApiService().postTarikSimpanan(argl = argl)
                     _tarikNominalSimpananResponse.value = response
                 }
             } catch (e: Exception) {
                 Log.e(TAG, "Failed to make a tarik simpnanan request ${e.message}")
+                e.printStackTrace()
             } finally {
                 _isLoading.value = false
             }
